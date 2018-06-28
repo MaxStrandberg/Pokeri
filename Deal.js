4c1518841df8c0b126f7af7aa9c76d8495b6b56a
MaxStrandberg <a1600552@myy.haaga-helia.fi>
2018-06-29 02:35:17 +0300

    Cleaned more code!

--- Deal.js
+++ Deal.js
@@ -1,20 +1,118 @@
-import Deck from './Deck.js';
+import Deck from 'Deck.js';
 
-//var cards = [];
+class Deal {
+  checkFlush(hand){
+      
+      
+    for (var y = 0; y < 5; y++){
+        
+        if(hand[0].substring(hand[0].length - 1) != hand[y].substring(hand[y].length - 1)){
+          return false;
+              }
+      }
+      
+    return true;
+  }
+
+  checkTwopair(hand){
+    var numbers = [];
+      for (var y = 0; y < hand.length ; y++){
+        numbers.push(hand[y].slice(0,2))  
+        }
+
+
+    var results = [];
+    
+    var sortedHand = numbers.slice().sort();
+      for (var i = 0; i < sortedHand.length -1; i++ ){
+          if (sortedHand[i+1] == sortedHand[i] && sortedHand[i+1] != sortedHand[i+2]){
+              results.push(sortedHand[i]);
+            }
+
+        }
+          if(results.length != 2){
+            return false
+              }
+    return true;
+  }
+
+  
+
+  checkStraight(hand){
+
+    var values = ['2 ', '3 ', '4 ', '5 ', '6 ', '7 ', '8 ', '9 ', '10', 'J ', 'Q ', 'K ','A '];
+    var numbers = [];
+    var results = [];
+      for (var y = 0; y < hand.length ; y++){
+          numbers.push(hand[y].slice(0,2)); 
+        }
+        
+        
+
+
+    
+    
+    var sortedHand = numbers.slice().sort((a,b) => values.indexOf(a) - values.indexOf(b));
+    let c0 = values.indexOf(sortedHand[0]);
+    let c4 = values.indexOf(sortedHand[4]);
+
+    for (var i = 0; i < sortedHand.length -1; i++ ){
+      if (sortedHand[i+1] == sortedHand[i]){
+            results.push(sortedHand[i]);
+              }
+      if(results.length != 0){
+            return false;
+              }
+    }
+      if(sortedHand[4] == 'A ' && sortedHand[3] == '5 ' ){
+        
+          return true;
+
+        }
+
+      if( c4-c0 == 4){
+        console.log(sortedHand);
+         return true;
+
+          }
+         
+        
+  }
+
+
+
+
+  evaluate(hand){
+
+        
+         if(this.checkStraight(hand) && this.checkFlush(hand)){
+           console.log("Straight Flush")
+          }
+          else if(this.checkFlush(hand)){
+            console.log("Flush")
+          }
+          else if(this.checkStraight(hand)){
+            console.log("Straight")
+          }   
+          else if(this.checkTwopair(hand)){
+            console.log("Two pair")
+          }
+      
+  }
+
+
+}
+
+var cards = [];
 const shuffled = new Deck();
 shuffled.shuffle()
 shuffled.deal()
-//cards.push(shuffled.deck.slice(0,15));
-// console.log(cards);
-  var p1 = [];
-  p1.push(shuffled.deck.slice(0,5));
-  var p2 = [];
-  p2.push(shuffled.deck.slice(5,10));
-  var p3 = [];
-  p3.push(shuffled.deck.slice(10,15));
- 
+const evaluate = new Deal();
 
 
-console.log(p1);
-console.log(p2);
-console.log(p3);
+  for (i=0; i <= 0; i++){
+    cards = shuffled.deck.splice(0,5);
+    console.log(cards);
+    shuffled.evaluate.evaluate(cards);
+    cards = [];
+  }
--- Deck.js
+++ Deck.js
@@ -1,157 +1,150 @@
 class Deck {
-   
-  constructor() {
-    this.deck = ['7 ❤', '6 ♣', '5 ❤', '4 ♣', '3 ♦'];
 
-    /*const suits = ['\u2660', '\u2666', '\u2663', '\u2764'];
+  constructor() {
+    this.deck = [];
+
+    const suits = ['\u2660', '\u2666', '\u2663', '\u2764'];
     const values = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
 
 
-    
+
     for (let suit in suits) {
 
       for (let value in values) {
-       
+
         this.deck.push(`${values[value]} ${suits[suit]}`);
       }
-    }*/
+    }
   }
 
-  
-  shuffle(){
-        const { deck } = this;
-        let i = deck.length;
-        let x;
-  
-      while(i){
-        x = Math.floor(Math.random() * i--);
-  
-        [deck[i], deck[x]] = [deck[x], deck[i]];
-      }
-  
-      return this;
-  }
-  
-  deal(){
-      return this.deck.pop();
+
+  shuffle() {
+    const { deck } = this;
+    let i = deck.length;
+    let x;
+
+    while (i) {
+      x = Math.floor(Math.random() * i--);
+
+      [deck[i], deck[x]] = [deck[x], deck[i]];
+    }
+
+    return this;
   }
 
-  checkFlush(hand){
-      
-      
-    for (var y = 0; y < 5; y++){
-        
-        if(hand[0].substring(hand[0].length - 1) != hand[y].substring(hand[y].length - 1)){
-          return false;
-              }
+  deal() {
+    return this.deck.pop();
+  }
+
+  checkFlush(hand) {
+
+
+    for (var y = 0; y < 5; y++) {
+
+      if (hand[0].substring(hand[0].length - 1) != hand[y].substring(hand[y].length - 1)) {
+        return false;
       }
-      
+    }
+
     return true;
   }
 
-  checkTwopair(hand){
+  checkTwopair(hand) {
     var numbers = [];
-      for (var y = 0; y < hand.length ; y++){
-        numbers.push(hand[y].slice(0,2))  
-        }
+    for (var y = 0; y < hand.length; y++) {
+      numbers.push(hand[y].slice(0, 2))
+    }
 
 
     var results = [];
-    
+
     var sortedHand = numbers.slice().sort();
-      for (var i = 0; i < sortedHand.length -1; i++ ){
-          if (sortedHand[i+1] == sortedHand[i] && sortedHand[i+1] != sortedHand[i+2]){
-              results.push(sortedHand[i]);
-            }
+    for (var i = 0; i < sortedHand.length - 1; i++) {
+      if (sortedHand[i + 1] == sortedHand[i] && sortedHand[i + 1] != sortedHand[i + 2]) {
+        results.push(sortedHand[i]);
+      }
 
-        }
-          if(results.length != 2){
-            return false
-              }
+    }
+    if (results.length != 2) {
+      return false
+    }
     return true;
   }
 
-  
 
-  checkStraight(hand){
 
-    var values = ['2 ', '3 ', '4 ', '5 ', '6 ', '7 ', '8 ', '9 ', '10', 'J ', 'Q ', 'K ','A '];
+  checkStraight(hand) {
+
+    var values = ['2 ', '3 ', '4 ', '5 ', '6 ', '7 ', '8 ', '9 ', '10', 'J ', 'Q ', 'K ', 'A '];
     var numbers = [];
     var results = [];
-      for (var y = 0; y < hand.length ; y++){
-          numbers.push(hand[y].slice(0,2)); 
-        }
-        
-        
+    for (var y = 0; y < hand.length; y++) {
+      numbers.push(hand[y].slice(0, 2));
+    }
 
-
-    
-    
-    var sortedHand = numbers.slice().sort((a,b) => values.indexOf(a) - values.indexOf(b));
+    var sortedHand = numbers.slice().sort((a, b) => values.indexOf(a) - values.indexOf(b));
     let c0 = values.indexOf(sortedHand[0]);
     let c4 = values.indexOf(sortedHand[4]);
 
-    for (var i = 0; i < sortedHand.length -1; i++ ){
-      if (sortedHand[i+1] == sortedHand[i]){
-            results.push(sortedHand[i]);
-              }
-      if(results.length != 0){
-            return false;
-              }
+    for (var i = 0; i < sortedHand.length - 1; i++) {
+      if (sortedHand[i + 1] == sortedHand[i]) {
+        results.push(sortedHand[i]);
+      }
+      if (results.length != 0) {
+        return false;
+      }
     }
-      if(sortedHand[4] == 'A ' && sortedHand[3] == '5 ' ){
-        
-          return true;
+    if (sortedHand[4] == 'A ' && sortedHand[3] == '5 ') {
 
-        }
+      return true;
 
-      if( c4-c0 == 4){
-        console.log(sortedHand);
-         return true;
+    }
 
-          }
-         
-        
+    if (c4 - c0 == 4) {
+      console.log(sortedHand);
+      return true;
+
+    }
+
+
   }
 
 
+  evaluate(hand) {
 
 
-  evaluate(hand){
+    if (this.checkStraight(hand) && this.checkFlush(hand)) {
+      console.log("Straight Flush")
+    }
+    else if (this.checkFlush(hand)) {
+      console.log("Flush")
+    }
+    else if (this.checkStraight(hand)) {
+      console.log("Straight")
+    }
+    else if (this.checkTwopair(hand)) {
+      console.log("Two pair")
+    }
 
-        
-         if(this.checkStraight(hand) && this.checkFlush(hand)){
-           console.log("Straight Flush")
-          }
-          else if(this.checkFlush(hand)){
-            console.log("Flush")
-          }
-          else if(this.checkStraight(hand)){
-            console.log("Straight")
-          }   
-          else if(this.checkTwopair(hand)){
-            console.log("Two pair")
-          }
-      
   }
 
 }
-  
+
 var cards = [];
 const shuffled = new Deck();
-//shuffled.shuffle()
-//shuffled.deal()
+shuffled.shuffle()
+shuffled.deal()
 
 
-  for (i=0; i <= 0; i++){
-    cards = shuffled.deck.splice(0,5);
-    console.log(cards);
-    shuffled.evaluate(cards);
-    cards = [];
-  }
- 
-  
-  
+for (i = 0; i <= 2; i++) {
+  cards = shuffled.deck.splice(0, 5);
+  console.log(cards);
+  shuffled.evaluate(cards);
+  cards = [];
+}
+
+
+
 
 
 
